2bad1d4ae7d8edaacc9779d6a08b9cbe34d8cb81
unknown <navnathdhekale9373@gmail.com>
2024-12-25 14:15:35 +0530

    xyz

--- giPproject.java
+++ giPproject.java
@@ -3,5 +3,8 @@ import java.util.*;
 class giPproject{
     public static void main(String[] args) {
         System.out.println("hello wolr");
+        for(int i=0;i<10;i++){
+            System.out.println(i*2);
+        }
     }
 }
